5db370a39e68f06197bcda8fadae801f02809697
mpinault <mpinault@users.noreply.github.com>
2015-05-30 14:38:14 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,1 +1,16 @@
 
+
+#include "Interface.h"
+
+
+int main(int argc, char *argv[])
+{
+    QApplication app(argc, argv);
+
+    Fenetre4 fenetre4;
+    fenetre4.show();
+
+    Fenetre10 fenetre10;
+    fenetre10.show();
+    return app.exec();
+}
