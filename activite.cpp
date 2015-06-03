84bb3ac1e70d63212334bb6db3f440073d4a974f
mpinault <mpinault@users.noreply.github.com>
2015-06-03 18:46:35 +0200

    Update activite.cpp

--- activite.cpp
+++ activite.cpp
@@ -1,1 +1,13 @@
 
+#include "activite.h"
+
+
+std::ostream& operator<<(std::ostream& f, const ActiviteTrad& a){
+    a.afficher(f);
+    return f;
+}
+
+std::ostream& operator<<(std::ostream& f, const TIME::Duree& d){
+    d.afficher(f);
+    return f;
+}
