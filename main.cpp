45e2c7b857c9931e0de77df38ac044d0ba8cfee1
mpinault <mpinault@users.noreply.github.com>
2015-06-13 15:36:05 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -7,7 +7,8 @@
 
 #include "Interface.h"
 #include "activite.h"
-
+#include "evtmanager.h"
+#include "actmanager.h"
 
 int main(int argc, char *argv[])
 {
@@ -16,28 +17,9 @@ int main(int argc, char *argv[])
 
 
         ProjetManager& pManager=ProjetManager::getInstance();
-        /*
-        qDebug()<<"2";
-
-        Projet& project = pManager.creerProjet(QString("Projet X1"),QDate(),QDate());
-        Projet& project2 = pManager.creerProjet(QString("Projet X2"),QDate(),QDate());
-        Projet& project3 = pManager.creerProjet(QString("Projet X3"),QDate(),QDate());
-        qDebug()<<"3";
-
-        qDebug()<<project.getTitre();
-        Tache& tache1 = project.creerTache(QString("Première tache"),QDate(),QDate(),Duree(),0);
-        Tache& tache2 = project.creerTache(QString("Deuxième tache"),QDate(),QDate(),Duree(),1);
-        Tache& tache3 = project.creerTache(QString("Troisième tache"),QDate(),QDate(),Duree(),2);
-        dynamic_cast<TacheComposite&>(tache3).ajouterTacheComp(tache2);
-        qDebug()<<"4";
-
-        qDebug()<<tache1.getTitre();
-        qDebug()<<typeid(tache1).name();
-        qDebug()<<typeid(tache2).name();
-        qDebug()<<typeid(tache3).name();
-
-        Fenetre19 fenetre19(project);
-        fenetre19.show();*/
+        EvtManager& mManager=EvtManager::getInstanceEvt();
+        ActManager& aManager=ActManager::getInstanceAct();
+     
 
         Fenetre1 fenetre1;
         fenetre1.show();
